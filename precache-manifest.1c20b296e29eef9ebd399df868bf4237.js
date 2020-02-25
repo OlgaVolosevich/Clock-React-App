7c487920ffb9a35d23c5ce05ea97034fc5874a44
Olga Volosevich <olga.lucky.v@gmail.com>
2020-02-25 22:37:26 +0300

    Updates

---
+++ precache-manifest.1c20b296e29eef9ebd399df868bf4237.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3196da931bfdc3a95b10aaa85bcde4a2",
+    "url": "/Clock-React-App/index.html"
+  },
+  {
+    "revision": "ac9c9ebd07d95220a181",
+    "url": "/Clock-React-App/static/css/main.d6b28c43.chunk.css"
+  },
+  {
+    "revision": "f9149191fc04857fcdc7",
+    "url": "/Clock-React-App/static/js/2.e4d0b45f.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Clock-React-App/static/js/2.e4d0b45f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac9c9ebd07d95220a181",
+    "url": "/Clock-React-App/static/js/main.32fca8e9.chunk.js"
+  },
+  {
+    "revision": "beba52db15e70a58b7fc",
+    "url": "/Clock-React-App/static/js/runtime-main.1fb84bc4.js"
+  }
+]);
